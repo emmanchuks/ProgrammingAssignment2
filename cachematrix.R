41aba8ea6a2ee66555c0920cc9673a2a1afc3f43
Ezeonyebuchi Emmanuel Chukwuma <26045664+emmanchuks@users.noreply.github.com>
2019-03-05 20:36:49 +0100

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -12,4 +12,19 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        
+    inv <- x$getinverse()
+  ##Checking if matrix is existing for avoiding re-calculation and returning the inverse
+    if(!is.null(inv)) {
+        message("Inverse already aviliable, Reading from Cache")
+        return(inv)
+    }
+  ##Assigning matrix x to a variable
+    var_mat <- x$get()
+  ##Solving for inverse
+    inv <- solve(var_mat, ...)
+  ##passing inverse of the matrix to setter
+    x$setinverse(inv)
+    inv
 }
+
